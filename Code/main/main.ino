4739270691bfe45847832a6413a9b6b7d135b018
danielg02 <danielgobalakrishnan@gmail.com>
2021-05-09 23:11:55 -0400

    Added function to Arduino code to get the chosen waveform from the button inputs

--- Code/main/main.ino
+++ Code/main/main.ino
@@ -1,4 +1,3 @@
-/R2R DAC. May 2018
 //Define DAC pins here
 #define DAC0 2
 #define DAC1 3
@@ -8,6 +7,11 @@
 #define DAC5 7
 #define DAC6 8
 #define DAC7 9
+
+#define SINE 0
+#define TRI 1
+#define SAW 2
+#define CUSTOM 3
 
 byte i;
 byte res = 100;   //Change this to the number of values in the array lookup tables
@@ -21,22 +25,32 @@ byte sinewave[200] = {0};
 byte generated[100];    //For the generated waveform (Experimental)
                    
 void setup() {
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A5, INPUT);
+
   generateSine(sinewave);
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); // Remove when adding SD functionality
   for(i=2;i<10;i++){pinMode(i,OUTPUT);}    //Set pins as outputs
 }
 
-
-void loop() {
-  byte *arr = sinewave; 
-  // put your main code here, to run repeatedly:
-  int arrSize = *(&arr + 1) - arr;
-  
-  for(i=0;i<arrSize;i++){        //Loop values
-    output(generated[i]);
-    delay(1);
+int getWaveShape() {
+  const int threshold = 700;
+  if (analogRead(A0)>threshold){
+    shape = SINE;
   }
+  else if (analogRead(A1)>threshold){
+    shape = TRI;
+  }
+  else if (analogRead(A2)>threshold){
+    shape = SAW;
+  }
+  else if (analogRead(A6)>threshold){
+    shape = CUSTOM;
+  }  
+  return shape;
 }
 
 //Write binary to output DAC pins
@@ -56,5 +70,16 @@ void generateSine(byte sinewave[])
 {
   for (int j = 0; j < 200; j++) {
     sinewave[j] = byte(sin(map(j, 0, 200, 0, PI)));
+  }
+}
+
+void loop() {
+  byte *arr = sinewave; 
+  // put your main code here, to run repeatedly:
+  int arrSize = *(&arr + 1) - arr;
+  
+  for(i=0;i<arrSize;i++){        //Loop values
+    output(generated[i]);
+    delay(1);
   }
 }
